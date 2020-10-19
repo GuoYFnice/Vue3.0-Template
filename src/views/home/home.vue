151b510d84bf94e2f8e55c1e004770a72c0d9476
GuoYFnice <603776827@qq.com>
2020-10-19 09:22:33 +0800

    鑿滃崟閰嶇疆鏂囦欢

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -15,24 +15,6 @@
           <a-menu-item key="3">option3</a-menu-item>
           <a-menu-item key="4">option4</a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template v-slot:title>
-            <span>subnav 2</span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template v-slot:title>
-            <span>subnav 3</span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
       </a-menu>
     </a-layout-sider>
     <a-layout style="padding: 0 24px 24px">

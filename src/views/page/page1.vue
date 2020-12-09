943d9b563ca016a69b7d954943769925616e6a8e
GuoYFnice <603776827@qq.com>
2020-12-09 16:17:13 +0800

    '为了提交！'

--- src/views/page/page1.vue
+++ src/views/page/page1.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
-    <Loading v-if="loading"/>
+    <Loading v-if="loading" />
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs} from "vue";
-import Loading from "../../components/Loading.vue"
-import handleLoading from "../../composables/handleLoading.js"
+import { ref, reactive, toRefs } from "vue";
+import Loading from "../../components/Loading.vue";
+import handleLoading from "../../composables/handleLoading.js";
 export default {
   components: {
-    Loading
+    Loading,
   },
   setup() {
-    const {loading, startLoading, endLoading} = handleLoading()
+    const { loading, startLoading, endLoading } = handleLoading();
     return {
       loading,
       startLoading,
-      endLoading
-    }
+      endLoading,
+    };
   },
-  mounted(){
-    let _this = this
-    this.startLoading()
+  mounted() {
+    let _this = this;
+    this.startLoading();
     setTimeout(() => {
-      _this.endLoading()
-    },2000)
-  }
+      _this.endLoading();
+    }, 2000);
+  },
 };
 </script>
 

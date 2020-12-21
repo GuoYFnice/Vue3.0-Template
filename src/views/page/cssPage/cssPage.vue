56567b055ad8704818224e238b45fe8de6883031
GuoYFnice <603776827@qq.com>
2020-12-21 14:56:53 +0800

    'feat：增加监听事件api'

--- src/views/page/cssPage/cssPage.vue
+++ src/views/page/cssPage/cssPage.vue
@@ -8,6 +8,7 @@
 </template>
 <script>
 import { ref, reactive,computed ,toRefs} from "vue";
+import handleDocumentEvent from "../../../composables/handleDocumentEvent"
 export default {
   name: "cssPage",
   setup() {
@@ -27,6 +28,9 @@ export default {
       callback
     }
   },
+  created(){
+    handleDocumentEvent("mousemove",()=>{console.log('点击了')},true)
+  }
 };
 </script>
 <style lang="scss" src="./style.scss" module/>

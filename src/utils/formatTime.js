f5b6919d10513bb266483f11caa34a9bbbf528db
GuoYFnice <603776827@qq.com>
2020-12-29 09:28:03 +0800

    feat- 增加一些工具方法 GuoYFnice

---
+++ src/utils/formatTime.js
@@ -0,0 +1,22 @@
+/* 
+  时间处理方法
+*/
+export function formatTime(fmt, date) {
+  // let fmt = "YYYYmmddHHMMSS";
+  let ret;
+  const opt = {
+    "Y+": date.getFullYear().toString(),
+    "m+": (date.getMonth() + 1).toString(),
+    "d+": date.getDate().toString(),
+    "H+": date.getHours().toString(),
+    "M+": date.getMinutes().toString(),
+    "S+": date.getSeconds().toString()
+  };
+  for (const k in opt) {
+    ret = new RegExp("(" + k + ")").exec(fmt);
+    if (ret) {
+      fmt = fmt.replace(ret[1], (ret[1].length === 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")));
+    }
+  }
+  return fmt;
+};
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+import formatTime from "./formatTime.js"
+import formatMoney from "./formatMoney.js"
+import utf8ToFile from "./utf8ToFile.js"
+
+export default {
+  formatTime,
+  formatMoney,
+  utf8ToFile
+}

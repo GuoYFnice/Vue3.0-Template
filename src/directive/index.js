3bda2aeaeb9e1febfc4a6081f705bab4c31b18fc
GuoYFnice <603776827@qq.com>
2021-05-28 17:09:23 +0800

    feat:Directives优化-GuoYF

---
+++ src/directive/index.js
@@ -0,0 +1,5 @@
+import { setuprightClickMenu } from './rightClickMenu'
+
+export function setupGlobDirectives(app) {
+  setuprightClickMenu(app)
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
-import Antd from 'ant-design-vue';
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'ant-design-vue/dist/antd.css';
+import { setupGlobDirectives } from './directive'
+import 'ant-design-vue/dist/antd.css'
 
 // 虚假的登录检测
 router.beforeEach((to, from, next) => {
@@ -13,45 +14,53 @@ router.beforeEach((to, from, next) => {
     }
   }
   next()
-});
+})
 
- 
+const app = createApp(App)
 
+setupGlobDirectives(app)
+
+app.use(router).use(store).use(Antd).mount('#app')
 
 // vue3.0使用createApp创建建VUE实例
 // 链式调用
-createApp(App).use(router).use(store).use(Antd).directive("rightClick", (el, binding) => {
-  el.oncontextmenu = function (e) {
-    const textArray = binding.value.text;
-    const handlerObj = binding.value.handle;
-    // 事件处理数组
-    const handlerArray = [];
-    // 处理好的右键菜单
-    const menuList = [];
-    // 将事件处理函数放入数组中
-    for (const key in handlerObj) {
-      handlerArray.push(handlerObj[key]);
-    }
-    // 追加右键菜单数据
-    for (let i = 0; i < textArray.length; i++) {
-      // 右键菜单对象, 添加名称
-      const menuObj = {
-        text: textArray[i],
-        handle: handlerArray[i],
-        id: i + 1
-      };
-      menuList.push(menuObj);
-    }
-    // 鼠标点的坐标
-    const oX = e.clientX;
-    const oY = e.clientY;
-    // 右键菜单出现后的位置
-    store.commit("updateRightMenuStatus", {
-      status: "block",
-      left: oX + "px",
-      top: oY + "px",
-      list: menuList
-    });
-    return false;
-  };
-}).mount('#app')
+// createApp(App)
+//   .use(router)
+//   .use(store)
+//   .use(Antd)
+//   .directive('rightClick', (el, binding) => {
+//     el.oncontextmenu = function (e) {
+//       const textArray = binding.value.text
+//       const handlerObj = binding.value.handle
+//       // 事件处理数组
+//       const handlerArray = []
+//       // 处理好的右键菜单
+//       const menuList = []
+//       // 将事件处理函数放入数组中
+//       for (const key in handlerObj) {
+//         handlerArray.push(handlerObj[key])
+//       }
+//       // 追加右键菜单数据
+//       for (let i = 0; i < textArray.length; i++) {
+//         // 右键菜单对象, 添加名称
+//         const menuObj = {
+//           text: textArray[i],
+//           handle: handlerArray[i],
+//           id: i + 1,
+//         }
+//         menuList.push(menuObj)
+//       }
+//       // 鼠标点的坐标
+//       const oX = e.clientX
+//       const oY = e.clientY
+//       // 右键菜单出现后的位置
+//       store.commit('updateRightMenuStatus', {
+//         status: 'block',
+//         left: oX + 'px',
+//         top: oY + 'px',
+//         list: menuList,
+//       })
+//       return false
+//     }
+//   })
+//   .mount('#app')
